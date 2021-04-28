aff49b7cf5730c2254eb9b3cd6765b5325564e00
Ali Rıza <ars.lifestyle@hotmail.com>
2021-04-28 16:10:19 +0300

    commit tree a53a68205fc3d48177af9cdf5eaa05eaaef0f0bd

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Code your solutions in this file
+
+function writeCards(arr, eventName) {
+  let empty = [];
+  for (let i = 0; i < arr.length; i++) {
+
+    empty.push("Thank you, " + arr[i] + ", for the wonderful " + eventName + " gift!");
+  }
+  return empty;
+}
